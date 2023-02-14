f429fb5245b5b48a1ce8b5634c21a058d90b55f0
fereydoon <epfereydoon@gmail.com>
2023-02-14 22:53:53 +0330

    update header

--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -1,5 +1,4 @@
 import React from "react";
-import { Container, Nav, Navbar, NavDropdown } from "react-bootstrap";
 import { Link } from "react-router-dom";
 import { useAuthState } from "../Contexts/AuthContext";
 import useDarkMode from "../hooks/useDarkMode";
@@ -46,42 +45,3 @@ const Header = () => {
   );
 };
 export default Header;
-
-
-
-    // <Navbar collapseOnSelect expand="lg">
-    // <Container>
-    //   <Navbar.Brand className="col-md-6 ">
-    //     <Link to="/" className="text-decoration-none px-1   text-color ">
-    //      React Shop
-    //     </Link>
-    //   </Navbar.Brand>
-    
-    //   <div className=" d-flex align-items-center  ">
-    //     <button type="button" onClick={toggleTheme} className="border border-0 dark-switch">
-    //       <svg className="moon" fill="none" stroke="currentColor" strokeLinecap="round" strokeLinejoin="round"
-    //         strokeWidth="2" width="24" height="24" viewBox="0 0 24 24">
-    //         <defs></defs>
-    //         <path d="M21 12.79A9 9 0 1111.21 3 7 7 0 0021 12.79z"></path>
-    //       </svg>
-    //     </button>
-    //     <Nav>
-    //       <Link to="/cart" className="text-decoration-none text-color mx-2">
-    //         🛒
-    //       </Link>
-    //     </Nav>
-    //     <>
-    //       {userEmail ? (
-    //         <NavDropdown title="Profile" id="collasible-nav-dropdown">
-    //           <NavDropdown.Item href="#">Dashboard</NavDropdown.Item>
-    //           <NavDropdown.Item href="#">Logout</NavDropdown.Item>
-    //         </NavDropdown>
-    //       ) : (
-    //         <Link to="/login" className="text-decoration-none text-color">
-    //           Login
-    //         </Link>
-    //       )}
-    //     </>
-    //   </div>
-    // </Container>
-    // </Navbar> 
